f24184c4ec727a667a716e335e38c1a2f5cee133
lazevedo <lazevedo@STFSAOC045860-L>
2023-03-11 17:40:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp_PrintPassOrFail/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp_PrintPassOrFail
+{
+    class Program
+    {
+        static void PrintPassOrFail(int score)
+        {
+            if (score >= 50) 
+            {
+                Console.WriteLine("Passou!");
+            }
+            else
+            {
+                Console.WriteLine("Falhou!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Programa Checar Score(Pontuação) ###");
+
+            Console.Write("Inserir Seu Nome completo: ");
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine($"\n\tSeja bem-vindo {nomeCompleto}.");
+
+            Console.Write("Inserir seu Score(pontuação): ");
+            string totalScore = Console.ReadLine();
+            int score = int.Parse(totalScore);
+
+            Console.WriteLine($"\n\tPontuação {score}.");
+
+            PrintPassOrFail(score);
+
+            Console.ReadKey();
+        }
+    }
+}
